b37baba7344b5cd47a7ee00ba00c1eba6bc96ec6
elkeye <sillyelk@keemail.me>
2025-09-07 16:28:24 +0300

    add micro, more fish abbreviations and TODO

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -1,3 +1,5 @@
 abbr -a -- cd z
 abbr -a -- ls eza
 abbr -a -- cat bat
+abbr -a -- nano micro
+abbr -a -- grep rg
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,5 @@ end
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
 zoxide init fish | source
+
+export "MICRO_TRUECOLOR=1"
